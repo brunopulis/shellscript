51f118e7ff01bb165f10b1b5cdadc5baf312e4f5
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-05-09 22:34:00 -0300

    primeiro script

---
+++ meuscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Meu comentário
+echo "Olá mundo!"
+
+# Uso de comentario de multiplas linhas
+# Podemos usar a notacao heredoc ":<<"
+
+:<<"COMMENT"
+Isso é um comentarios de multiplas
+linhas
+COMMENT
+
+echo "Inicio"
